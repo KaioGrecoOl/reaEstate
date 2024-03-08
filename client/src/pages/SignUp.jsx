c6386f765def165f222e55e2e36ccda81c9df795
Kaio G <kaiogreco91@gmail.com>
2024-03-08 18:27:41 -0300

    sign in page complete

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,70 +1,74 @@
 import { useState } from "react";
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom";
 
-export default function Signout() {
+export default function SignUp() {
   const [formData, setFormData] = useState({});
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
-
-  const handlerChange = (event) => {
-    setFormData({ ...formData, [event.target.id]: event.target.value });
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
   };
-
-  const handlerSubmit = async (event) => {
-    event.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     try {
       setLoading(true);
-      const res = await fetch('/api/auth/signup',
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(formData),
-        });
-        const data = await res.json();
-        if(data.success === false) {
-          setLoading(false);
-          setError(data.message);
-          return;
-        }
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.success === false) {
         setLoading(false);
-        setError(null);
-        navigate('/sign-in');
-    } catch (err) {
+        setError(data.message);
+        return;
+      }
       setLoading(false);
-      setError(err.message);
+      setError(null);
+      navigate("/sign-in");
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
     }
   };
-
   return (
     <div className="p-3 max-w-lg mx-auto">
-      <h1 className="text-3xl text-center font-semibold my-7">Signup</h1>
-      <form onSubmit={handlerSubmit} className="flex flex-col gap-4">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
-          id="username"
           type="text"
           placeholder="username"
           className="border p-3 rounded-lg"
-          onChange={handlerChange}
+          id="username"
+          onChange={handleChange}
         />
         <input
-          id="email"
           type="email"
           placeholder="email"
           className="border p-3 rounded-lg"
-          onChange={handlerChange}
+          id="email"
+          onChange={handleChange}
         />
         <input
-          id="password"
           type="password"
           placeholder="password"
           className="border p-3 rounded-lg"
-          onChange={handlerChange}
+          id="password"
+          onChange={handleChange}
         />
-        <button disabled={loading} className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-          {loading ? "Loading..." : "Signup"}
+
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading..." : "Sign Up"}
         </button>
       </form>
       <div className="flex gap-2 mt-5">

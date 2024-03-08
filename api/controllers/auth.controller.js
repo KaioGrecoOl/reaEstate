519270b20f274d6c92d12537d2c7b9a46950d0c7
Kaio G <kaiogreco91@gmail.com>
2024-03-08 19:54:30 -0300

    using redux persist to store data

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -22,12 +22,14 @@ export const signin = async (req, res, next) => {
     if (!validUser) return next(errorHandler(404, "User not found!"));
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) return next(errorHandler(401, "Wrong credentials!"));
-    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
-    const { password: pass, ...rest } = validUser._doc;
-    res
-      .cookie("access_token", token, { httpOnly: true })
-      .status(200)
-      .json(rest);
+    jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, (err, token) => {
+      if (err) return next(errorHandler(500, "Internal server error"));
+      const { password: pass, ...rest } = validUser._doc;
+      res
+        .cookie("access_token", token, { httpOnly: true })
+        .status(200)
+        .json(rest);
+    });
   } catch (error) {
     next(error);
   }

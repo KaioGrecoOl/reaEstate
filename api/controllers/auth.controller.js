c72580b9acb1b60a6e092ecaece2a5903631558a
Kaio G <kaiogreco91@gmail.com>
2024-03-08 18:01:28 -0300

    complete sign in functionality and creating access token for session

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
+import { userInfo } from "os";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -8,6 +11,23 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json({ message: "User created successfully" });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if(!user) return next(errorHandler(404, "User not found"));
+    const isMatch = bcryptjs.compareSync(password, user.password);
+    if(!isMatch) return next(errorHandler(401, "Wrong credentials!"));
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+    const { password: pass, ...userInfo } = user._doc;
+    res.cookie("access_token", token, {
+      httpOnly: true,
+    }).status(200).json(userInfo);
   } catch (err) {
     next(err);
   }

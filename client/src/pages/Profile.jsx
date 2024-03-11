0a4a6ff6326575a46bba1075a236a16076c5ee98
Kaio G <kaiogreco91@gmail.com>
2024-03-11 11:39:19 -0300

    progress of upload

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,5 +1,7 @@
 import { useSelector } from "react-redux";
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
+import { getStorage, ref, uploadBytesResumable } from "firebase/storage";
+import { app } from "../firebase";
 
 export default function Profile() {
   const { currentUser } = useSelector((state) => state.user);
@@ -7,13 +9,36 @@ export default function Profile() {
   const [file, setFile] = useState(undefined);
   console.log(file);
 
+  useEffect(() => {
+    if (file) {
+      handleFileUpload(file);
+    }
+  }, [file]);
+
+  const handleFileUpload = () => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on("state_changed", (snapshot) => {
+      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      console.log("Upload is " + progress + "% done");
+    });
+  };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
       <form className="flex flex-col gap-4">
-        <input onChange={(e) => setFile(e.target.files[0])} ref={fileRef} type="file" hidden accept="image/*"/>
+        <input
+          onChange={(e) => setFile(e.target.files[0])}
+          ref={fileRef}
+          type="file"
+          hidden
+          accept="image/*"
+        />
         <img
-        onClick={() => fileRef.current.click()}
+          onClick={() => fileRef.current.click()}
           src={currentUser.avatar}
           alt="profile"
           className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"

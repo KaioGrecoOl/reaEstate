90dcb44da595dcaff067cc40f633bf17fb59752c
Kaio G <kaiogreco91@gmail.com>
2024-03-10 22:26:04 -0300

    complete back end authentication with google

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,18 +1,33 @@
 import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
 import { app } from '../firebase';
-// import { useDispatch } from 'react-redux';
-// import { signInSuccess } from '../redux/user/userSlice';
+import { useDispatch } from 'react-redux';
+import { signInSuccess } from '../redux/user/userSlice';
+import { useNavigate } from 'react-router-dom';
 
 export default function OAuth() {
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
   const handlerGoogleClick = async () => {
     try {
       const provider = new GoogleAuthProvider()
       const auth = getAuth(app);
 
       const result = await signInWithPopup(auth, provider);
-
-      console.log(result.user);
+      const res = await fetch('/api/auth/google', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name: result.user.displayName,
+          email: result.user.email,
+          photo: result.user.photoURL,
+        }),
+      });
+      const data = await res.json();
+      dispatch(signInSuccess(data));
+      navigate('/');
     } catch (error) {
       console.log('Could not sign in with Google.', error)
     }

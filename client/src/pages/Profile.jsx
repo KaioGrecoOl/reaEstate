24e8fc787bb7273b8946c5c6e39e3ea07a30a0bb
Kaio G <kaiogreco91@gmail.com>
2024-03-11 11:14:58 -0300

    fix css profile page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -3,13 +3,13 @@ import { useSelector } from "react-redux";
 export default function Profile() {
   const { currentUser } = useSelector((state) => state.user);
   return (
-    <div>
+    <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
-      <form className="flex flex-col">
+      <form className="flex flex-col gap-4">
         <img
-          className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
           src={currentUser.avatar}
           alt="profile"
+          className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
         />
         <input
           type="text"
@@ -30,7 +30,7 @@ export default function Profile() {
           className="border p-3 rounded-lg"
         />
         <button className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">
-          Update
+          update
         </button>
       </form>
       <div className="flex justify-between mt-5">

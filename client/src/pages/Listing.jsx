aa69b1d38ba726d3c157f13697995eddacdab48f
Kaio G <kaiogreco91@gmail.com>
2024-03-27 16:46:02 -0300

    install Swiper and config to listing page

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,7 +1,12 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore from 'swiper';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css/bundle';
 
 export default function Listing() {
+  SwiperCore.use([Navigation]);
   const params = useParams()
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(false);
@@ -29,13 +34,27 @@ export default function Listing() {
   }, [params.listingId])
   return (
     <main>
-      {loading && <p className='text-center my-7 text-2xl'>Loading...</p>}
-      {error && (
-        <p className='text-center my-7 text-2xl'>Something went wrong!</p>
-      )}
-      {listing && !loading && !error && (
-        <h1>{listing.name}</h1>
-      )}
-    </main>
+    {loading && <p className='text-center my-7 text-2xl'>Loading...</p>}
+    {error && (
+      <p className='text-center my-7 text-2xl'>Something went wrong!</p>
+    )}
+    {listing && !loading && !error && (
+      <div>
+        <Swiper navigation>
+          {listing.imageUrls.map((url) => (
+            <SwiperSlide key={url}>
+              <div
+                className='h-[550px]'
+                style={{
+                  background: `url(${url}) center no-repeat`,
+                  backgroundSize: 'cover',
+                }}
+              ></div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    )}
+  </main>
   )
 }

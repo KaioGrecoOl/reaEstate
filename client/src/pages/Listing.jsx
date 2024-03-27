0109452684ff026a964cc5e7242d3d7f06a80b6d
Kaio G <kaiogreco91@gmail.com>
2024-03-27 17:06:26 -0300

    create funcionality to copy url

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,16 +1,18 @@
-import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
-import { Swiper, SwiperSlide } from 'swiper/react';
-import SwiperCore from 'swiper';
-import { Navigation } from 'swiper/modules';
-import 'swiper/css/bundle';
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore from "swiper";
+import { Navigation } from "swiper/modules";
+import "swiper/css/bundle";
+import { FaShare } from "react-icons/fa";
 
 export default function Listing() {
   SwiperCore.use([Navigation]);
-  const params = useParams()
+  const params = useParams();
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+  const [copied, setCopied] = useState(false);
   useEffect(() => {
     const fetchListing = async () => {
       try {
@@ -30,31 +32,36 @@ export default function Listing() {
         setLoading(false);
       }
     };
-    fetchListing()
-  }, [params.listingId])
+    fetchListing();
+  }, [params.listingId]);
   return (
     <main>
-    {loading && <p className='text-center my-7 text-2xl'>Loading...</p>}
-    {error && (
-      <p className='text-center my-7 text-2xl'>Something went wrong!</p>
-    )}
-    {listing && !loading && !error && (
-      <div>
-        <Swiper navigation>
-          {listing.imageUrls.map((url) => (
-            <SwiperSlide key={url}>
-              <div
-                className='h-[550px]'
-                style={{
-                  background: `url(${url}) center no-repeat`,
-                  backgroundSize: 'cover',
-                }}
-              ></div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
-    )}
-  </main>
-  )
+      {loading && <p className="text-center my-7 text-2xl">Loading...</p>}
+      {error && (
+        <p className="text-center my-7 text-2xl">Something went wrong!</p>
+      )}
+      {listing && !loading && !error && (
+        <div>
+          <Swiper navigation>
+            {listing.imageUrls.map((url) => (
+              <SwiperSlide key={url}>
+                <div
+                  className="h-[550px]"
+                  style={{
+                    background: `url(${url}) center no-repeat`,
+                    backgroundSize: "cover",
+                  }}
+                ></div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+          <div className="fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer">
+            <FaShare className='text-slate-500'
+            onClick={() => {
+              navigator.clipboard.writeText(window.location.href) }} />
+          </div>
+        </div>
+      )}
+    </main>
+  );
 }

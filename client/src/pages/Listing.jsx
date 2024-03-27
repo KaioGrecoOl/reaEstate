c2abf0a9a27890f4fd904bb67d1c9e68054790c7
Kaio G <kaiogreco91@gmail.com>
2024-03-27 18:12:25 -0300

    message when link is copied

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -56,10 +56,22 @@ export default function Listing() {
             ))}
           </Swiper>
           <div className="fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer">
-            <FaShare className='text-slate-500'
-            onClick={() => {
-              navigator.clipboard.writeText(window.location.href) }} />
+            <FaShare
+              className="text-slate-500"
+              onClick={() => {
+                navigator.clipboard.writeText(window.location.href);
+                setCopied(true);
+                setTimeout(() => {
+                  setCopied(false);
+                }, 200);
+              }}
+            />
           </div>
+          {copied && (
+            <p className="fixed top-[23%] right-[5%] z-10 rounded-md bg-slate-100 p-2">
+              Link copied
+            </p>
+          )}
         </div>
       )}
     </main>

d09445f3bd75de2949f23fc35308b0dd0c09a0be
Kaio G <kaiogreco91@gmail.com>
2024-03-27 18:52:36 -0300

    add new condition for the offer and discount

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -88,6 +88,11 @@ export default function Listing() {
             <p className='bg-red-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
                 {listing.type === 'rent' ? 'For Rent' : 'For Sale'}
               </p>
+              {listing.offer && (
+                <p className='bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
+                  ${+listing.regularPrice - +listing.discountPrice}
+                </p>
+              )}
             </div>
           </div>
         </div>
